31748c602c069a41b7f548914964a5f801d8575d
thaycacac <thaycacac@gmail.com>
2020-04-04 15:13:08 +0700

    update: after review

--- lecture 6/Practical/AnalysisQueryDW.sql
+++ lecture 6/Practical/AnalysisQueryDW.sql
@@ -1,41 +1,25 @@
-﻿/* Phân tích tổng số lượng bán được và tổng chi phí vận
-   chuyển theo Product Category */
-
-SELECT Dim_ProductCategory.Category AS ProductCategory, SUM(SoldProductQty) AS SoldQuantity, SUM(Freight) AS Freight
-FROM Facts
-INNER JOIN Dim_ProductCategory
-ON Facts.ProductCategoryID = Dim_ProductCategory.id
-GROUP BY Dim_ProductCategory.Category
-
-/* Phân tích tổng số lượng bán được và tổng chi phí vận
-   chuyển theo City */
-
-SELECT Dim_Customer.City, SUM(SoldProductQty) AS SoldQuantity, SUM(Freight) AS Freight
-FROM Facts
-INNER JOIN Dim_Customer
-ON facts.CustomerID = Dim_Customer.id
-WHERE facts.CustomerID IS NOT NULL
-GROUP BY Dim_Customer.City
-
-
-/* Phân tích tổng số lượng bán được và tổng chi phí vận
-   chuyển theo ProductCategory theo thời gian bán 
-   hàng (năm và tháng) */
-
-SELECT Dim_Time.Year, Dim_Time.Month, SUM(SoldProductQty) AS SoldQuantity, SUM(Freight) AS Freight
-FROM Facts
-INNER JOIN Dim_Time
-ON Dim_Time.id = facts.OrderTimeID
-GROUP BY Dim_Time.YEAR, Dim_Time.MONTH
-
-/* Phân tích tổng số lượng bán được và tổng chi phí vận
-   chuyển theo SaleTerritory */
-
-SELECT 
-  territory.Name, 
-  SUM(SoldProductQty) AS SoldQuantity, 
-  SUM(Freight) AS Freight
-FROM Facts as facts
-INNER JOIN Dim_Sale_Territory AS territory
-  ON territory.id = facts.TerritoryID
-GROUP BY facts.TerritoryID, territory.Name
+SELECT
+	Dim_ProductCategory.Category,
+	Dim_Customer.City,
+	Dim_Time.Year,
+	Dim_Time.Month,
+	Dim_Sale_Territory.Name,
+	sum(Facts.SoldProductQty) as SoldProductQty,
+	sum(Facts.Freight) as Freight
+FROM Facts 
+INNER JOIN Dim_Customer ON Facts.CustomerID = Dim_Customer.id
+LEFT JOIN Dim_Sale_Territory ON Facts.TerritoryID = Dim_Sale_Territory.id 
+LEFT JOIN Dim_Time ON Facts.OrderTimeID = Dim_Time.id
+INNER JOIN Dim_ProductCategory ON Facts.ProductCategoryID = Dim_ProductCategory.id
+GROUP BY
+	Dim_Customer.City,
+	Dim_Time.Year,
+	Dim_Time.Month,
+	Dim_Sale_Territory.Name,
+	Dim_ProductCategory.Category
+ORDER BY
+	Dim_ProductCategory.Category,
+	Dim_Customer.City,
+	Dim_Time.Year,
+	Dim_Time.Month,
+	Dim_Sale_Territory.Name
--- lecture 6/Practical/AnalysisQueryOP.sql
+++ lecture 6/Practical/AnalysisQueryOP.sql
@@ -1,63 +1,27 @@
-﻿/* Phân tích tổng số lượng bán được và tổng chi phí vận
-   chuyển theo Product Category */
-
 SELECT 
-  prd_sub_cat.ProductCategoryID,
-  SUM(orderDetails.OrderQty) AS SoldProductQty, 
-  SUM(orderHeader.Freight) AS Freight
-FROM Sales.SalesOrderDetail AS orderDetails
-INNER JOIN Sales.SalesOrderHeader AS orderHeader
-  ON orderDetails.SalesOrderID = orderHeader.SalesOrderID
-INNER JOIN Production.Product AS product_op
-  ON product_op.ProductID = orderDetails.ProductID
-INNER JOIN Production.ProductSubcategory AS prd_sub_cat
-  ON prd_sub_cat.ProductSubcategoryID = product_op.ProductSubcategoryID
-GROUP BY prd_sub_cat.ProductCategoryID
-ORDER BY prd_sub_cat.ProductCategoryID
-
-/* Phân tích tổng số lượng bán được và tổng chi phí vận
-   chuyển theo City */
-
-SELECT 
-  YEAR(orderHeader.OrderDate) AS target_year,
-  MONTH(orderHeader.OrderDate) AS target_month,
-  SUM(orderDetails.OrderQty) AS SoldProductQty, 
-  SUM(orderHeader.Freight) AS Freight
-FROM Sales.SalesOrderDetail AS orderDetails
-INNER JOIN Sales.SalesOrderHeader AS orderHeader
-  ON orderDetails.SalesOrderID = orderHeader.SalesOrderID
-INNER JOIN Production.Product AS product_op
-  ON product_op.ProductID = orderDetails.ProductID
-INNER JOIN Production.ProductSubcategory AS prd_sub_cat
-  ON prd_sub_cat.ProductSubcategoryID = product_op.ProductSubcategoryID
-GROUP BY YEAR(orderHeader.OrderDate), MONTH(orderHeader.OrderDate)
-
-
-/* Phân tích tổng số lượng bán được và tổng chi phí vận
-   chuyển theo ProductCategory theo thời gian bán 
-   hàng (năm và tháng) */
-
-SELECT 
-  customer_info.City AS City,
-  SUM(orderDetails.OrderQty) AS SoldProductQty, 
-  SUM(orderHeader.Freight) AS Freight
-FROM Sales.SalesOrderDetail AS orderDetails
-INNER JOIN Sales.SalesOrderHeader AS orderHeader
-  ON orderDetails.SalesOrderID = orderHeader.SalesOrderID
+	productCategory.Name AS ProductCategory,
+	customer_info.City AS City,
+	YEAR(orderHeader.OrderDate) AS [year],
+	MONTH(orderHeader.OrderDate) AS [month],
+	saleTerritory.Name,
+	SUM(orderDetails.OrderQty) AS SoldProductQty, 
+	SUM(orderHeader.Freight) AS Freight
+FROM Sales.SalesOrderDetail as orderDetails
+LEFT JOIN Sales.SalesOrderHeader AS orderHeader
+	ON orderDetails.SalesOrderID = orderHeader.SalesOrderID
 INNER JOIN Sales.vIndividualCustomer AS customer_info
-  ON customer_info.CustomerID = orderHeader.CustomerID
-GROUP BY customer_info.City
-
-/* Phân tích tổng số lượng bán được và tổng chi phí vận
-   chuyển theo SaleTerritory */
-
-SELECT 
-  territory.Name AS Territory,
-  SUM(orderDetails.OrderQty) AS SoldProductQty, 
-  SUM(orderHeader.Freight) AS Freight
-FROM Sales.SalesOrderDetail AS orderDetails
-INNER JOIN Sales.SalesOrderHeader AS orderHeader
-  ON orderDetails.SalesOrderID = orderHeader.SalesOrderID
-INNER JOIN Sales.SalesTerritory AS territory
-  ON territory.TerritoryID = orderHeader.TerritoryID
-GROUP BY territory.Name
+	ON customer_info.CustomerID = orderHeader.CustomerID
+LEFT JOIN Sales.SalesTerritory as saleTerritory
+	ON orderHeader.TerritoryID = saleTerritory.TerritoryID
+LEFT JOIN Production.Product AS product
+	ON product.ProductID = orderDetails.ProductID
+LEFT JOIN Production.ProductSubcategory AS productSubCategory
+	ON productSubCategory.ProductSubcategoryID = product.ProductSubcategoryID
+LEFT JOIN Production.ProductCategory AS productCategory
+	ON productSubCategory.ProductCategoryID = productCategory.ProductCategoryID
+GROUP BY
+	productCategory.Name,
+	customer_info.City,
+	YEAR(orderHeader.OrderDate),
+	MONTH(orderHeader.OrderDate),
+	saleTerritory.Name
